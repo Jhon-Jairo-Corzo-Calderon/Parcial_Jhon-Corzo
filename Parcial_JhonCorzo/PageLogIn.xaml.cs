0b5ae3c1c343a877614a9605fdb952955a8bf36d
Jhon-Jairo-Corzo-Calderon <jhon.corzo.2020.@upb.edu.co>
2020-09-04 17:22:23 -0500

    Punto 1, vista de login completada, esta dirige hacia la vista de admin o empleado, dependiendo de las credenciales.

---
+++ Parcial_JhonCorzo/PageLogIn.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Parcial_JhonCorzo
+{
+    /// <summary>
+    /// Interaction logic for PageLogIn.xaml
+    /// </summary>
+    public partial class PageLogIn : Page
+    {
+        public PageLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            if (usuario == "admin" && password == "1234")
+            {
+                MainWindow w = (MainWindow)Window.GetWindow(this);
+                w.mainFrame.NavigationService.Navigate(new PageAdmin());
+            }
+            else
+            {
+                MessageBox.Show("Invalid Credentials");
+            }
+
+
+            if (((usuario == "empleado1") || (usuario == "empleado2")) && txtPassword.Password == "1234")
+            {
+                MainWindow w = (MainWindow)Window.GetWindow(this);
+                w.mainFrame.NavigationService.Navigate(new PageEmpleado());
+            }
+            else
+            {
+                MessageBox.Show("Invalid Credentials");
+            }
+        }
+    }
+}
